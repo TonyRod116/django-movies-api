8c99c6b94e0464f42c447c3e79b1d2898299935c
TonyRod116 <tony.rod.bcn@gmail.com>
2025-08-21 21:51:44 +0200

    finished setup

--- project/settings.py
+++ project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+AUTH_USER_MODEL='users.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
