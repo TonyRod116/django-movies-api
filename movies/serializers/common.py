5f837b23aec6d9d2bfe9082795532fffacf4849a
TonyRod116 <tony.rod.bcn@gmail.com>
2025-08-21 22:19:12 +0200

    Fixed serializers package and server issues

--- movies/serializers/common.py
+++ movies/serializers/common.py
@@ -1,7 +1,7 @@
-from rest_framework import serializers
-from movies.models import Movie
+from rest_framework.serializers import ModelSerializer
+from ..models import Movie
 
-class MovieSerializer(serializers.ModelSerializer):
+class MovieSerializer(ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
