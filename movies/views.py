793df5f0360d8874ea96eeb618754596b7a79084
TonyRod116 <tony.rod.bcn@gmail.com>
2025-08-21 22:15:59 +0200

    Fixed server issues: added missing imports and serializers

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Movie
+from .serializers.common import MovieSerializer
 
 # Create your views here.
+# Patch: /countries/
+class MovieView(APIView):
+
+    # Index route
+    # Method: GET
+    def get(self, request):
+        movies = Movie.objects.all()
+        serialized_movies = MovieSerializer(movies, many=True)
+        return Response(serialized_movies.data)
+    
+    # Create route
+    # Method: POST
+    def post(self, request):
+        return Response("HIT CREATE ROUTE")
